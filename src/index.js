7fdc06c1005143c859472bbcab2517a5950aec43
VarunKumarPrajapati <varunkumarprajapati57@gmail.com>
2024-10-18 20:21:52 +0530

    setup express

---
+++ src/index.js
@@ -0,0 +1,11 @@
+require("dotenv");
+require("express-async-errors");
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("<h1>Home Page</h1>");
+});
+
+app.listen(process.env.PORT);
