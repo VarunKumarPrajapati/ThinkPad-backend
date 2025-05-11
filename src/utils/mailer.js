5281d939542945577c12cede4f95389ea631cfdb
VarunKumarPrajapati <varunkumarprajapati57@gmail.com>
2025-05-11 17:38:40 +0530

    change(mailer):using less secure app for gmail

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,13 +1,23 @@
 const nm = require("nodemailer");
 
+// const transporter = nm.createTransport({
+//   service: "gmail",
+//   auth: {
+//     type: "OAuth2",
+//     user: process.env.GMAIL_EMAIL,
+//     clientId: process.env.GMAIL_CLIENT_ID,
+//     clientSecret: process.env.GMAIL_CLIENT_SECRET,
+//     refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+//   },
+// });
+
 const transporter = nm.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false,
   auth: {
-    type: "OAuth2",
-    user: process.env.GMAIL_EMAIL,
-    clientId: process.env.GMAIL_CLIENT_ID,
-    clientSecret: process.env.GMAIL_CLIENT_SECRET,
-    refreshToken: process.env.GMAIL_REFRESH_TOKEN,
+    user: process.env.EMAIL_ID,
+    pass: process.env.EMAIL_PASSWORD,
   },
 });
 
